fcd03c2cdd3e5e534c5dbf8628f599fcb41cebcc
Nicholas C Rozzi <nrozzi1@purdue.edu>
2024-03-11 16:37:26 -0400

    Unit 2 Assignment Submission

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(268, 149);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Connect";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(271, 288);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 20);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Count";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(271, 425);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(150, 20);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Company Names";
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(486, 138);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(176, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Connect to DB";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(486, 288);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(176, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Get Customer Count";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(486, 425);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(176, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Get Customer Names";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(992, 574);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label Connect;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,121 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace IT481_Unit_2_Assignment_Nick_Rozzi
 {
     public partial class Form1 : Form
     {
+        DB database;
+        string dbConnString = "Server = localhost\\SQLEXPRESS; Trusted_Connection=true; Database=northwind; User Instance=false; TrustServerCertificate=True; Connection timeout=30;";
+        bool databaseCreated = false;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            database = new DB(dbConnString);
+            databaseCreated = true;
+            MessageBox.Show("Connection Information Sent");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (databaseCreated == false)
+            {
+                database = new DB(dbConnString);
+                databaseCreated = true;
+            }
+            string count = database.getCustomerCount();
+            MessageBox.Show(count);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (databaseCreated == false)
+            {
+                database = new DB(dbConnString);
+                databaseCreated = true;
+            }
+            string companyNames = database.getCompanyNames();
+            MessageBox.Show(companyNames);
+        }
+    }
+
+    class DB
+    {
+        string connectionString;
+
+        public DB()
+        {
+            connectionString = "Server = localhost\\SQLEXPRESS; Trusted_Connection=true; Database=northwind; User Instance=false; TrustServerCertificate=True; Connection timeout=30;";
+        }
+
+        public DB(string pConnString)
+        {
+            connectionString = pConnString;
+        }
+
+        public string getCustomerCount()
+        {
+            int count = 0;
+            string countQuery = "SELECT COUNT(*) FROM customers;";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(countQuery, connection))
+            {
+                command.CommandType = CommandType.Text;
+
+                if (connection.State == ConnectionState.Closed) connection.Open();
+
+                try
+                {
+                    count = Convert.ToInt32(command.ExecuteScalar());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return count.ToString();
+        }
+
+        public string getCompanyNames()
+        {
+            string names = string.Empty;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand("SELECT companyName FROM customers;", connection))
+            {
+                command.CommandType = CommandType.Text;
+
+                if (connection.State == ConnectionState.Closed) connection.Open();
+
+                try
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                names += reader[0].ToString() + "\n";
+                            }
+                        }
+                        else
+                        {
+                            names = "None";
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return names;
         }
     }
 }
